c064f3123c06e4a221ec3a9ebffa33eb638ec1a4
Isaiah Omame <omameazy@yahoo.com>
2022-10-09 19:53:43 -0700

    Deploying Card Micro Service to Jenkins

---
+++ src/main/java/com/micro/card/service/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.micro.card.service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MicrocardserviceApplication.class);
+    }
+
+}
